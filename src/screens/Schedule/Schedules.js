46af1d5096b4ad41cab5be131d3ffe29c81246cc
Keilla Vitória <keillavfb@gmail.com>
2022-12-14 17:23:41 -0300

    Updated screens

--- src/screens/Schedule/Schedules.js
+++ src/screens/Schedule/Schedules.js
@@ -1,26 +1,68 @@
 import React from 'react';
 import './Schedules.css';
 
+import SchedulingTable from "../../components/SchedulingTable";
+import SchedulingService from "../../services/SchedulingService";
+
 export default class Schedules extends React.Component {
+
+    state = {
+        scheduling: []
+    }
+
+    constructor() {
+        super();
+        this.service = new SchedulingService();
+    }
+
+    find = () => {
+        this.service.find('')
+        .then( Response => {
+            const scheduling = Response.data;
+            this.setState({scheduling: scheduling});
+            console.log(scheduling);
+        }).catch( error => {
+            console.log(error.Response)
+        });
+    }
+
+    delete = (schedulingId) => {
+        this.service.delete(schedulingId)
+        .then( Response => {
+            this.find();
+        }).catch( error => {
+            console.log(error.Response)
+        });
+    }
+
+    scheduleValidation = () => {
+        this.props.history.push("/scheduling/create");
+    }
+
+    componentDidMount() {
+        this.find();
+    }
 
     render() {
         return(
 
-            <div className= "Schedules01">
-            <h1>Lattes</h1>
-            <h2>++</h2>
-            
+            <div className= "Principal">
+                <div className= "ScheduleValidation01">
+                    <h1>Lattes</h1>
+                    <h2>++</h2>
 
-            <button type="importresume" class="b1">Importar currículo</button>
-            <button type="schedulevalidation" class="b2">Agendar validação</button>
-            <button type="Schedules" class="b3">Agendamentos</button>
-            <button type="versions" class="b4">Versões</button>
-            <button type="export" class="b5">Exportar</button>
-            <button type="goout" class="b6">Sair</button>
-            
-            
+                    <button type="importresume" class="b1">Importar currículo</button>
+                    <button onClick={this.scheduleValidation} type="schedulevalidation" class="b2">Agendar validação</button>
+                    <button type="Schedules" class="b3">Agendamentos</button>
+                    <button type="versions" class="b4">Versões</button>
+                    <button type="export" class="b5">Exportar</button>
+                    <button type="goout" class="b6">Sair</button>
+                </div>
+
+                <div className="Fields">
+                    <SchedulingTable schedulings={this.state.scheduling} delete={this.delete}/>
+                </div>
             </div>
-
         )
 
     }
