dec7b3f94335c6982a8edb89e7b187c82213e476
DanSousa <dansousac2@gmail.com>
2022-12-19 15:00:21 -0300

    comunication of to solicit scheduling in front with back done

--- src/screens/schedulevalidation/ScheduleValidation.js
+++ src/screens/schedulevalidation/ScheduleValidation.js
@@ -8,9 +8,11 @@ export default class ScheduleValidation extends React.Component {
 
     state = {
         version:"",
-        validator:"",
+        requesterId:"",
+        validatorId:"",
+        address:"",
         date:"",
-        time:""
+        time:"",
     }
 
     constructor() {
@@ -21,20 +23,24 @@ export default class ScheduleValidation extends React.Component {
     post = () => {
         this.service.create(
             {
-               name: this.state.name,
-               validator: this.state.validator,
-               date: this.state.date,
-               time: this.state.time
+                version:this.state.version,
+                requesterId:this.state.requesterId,
+                validatorId:this.state.validatorId,
+                date:this.state.date,
+                time:this.state.time,
+                address:this.state.address,
+                status:"OPEN"
             }
-       ) .then( response =>
-           {
-                // Adicionar mensagem de sucesso
-           }
-       ).catch(error =>
-           {
-               console.log(error.response);
-           }
-       );
+            ) .then( response =>
+                {
+                    // Adicionar mensagem de sucesso
+                    console.log("Cadastrado com sucesso!!");
+                }
+                ).catch(error =>
+                    {
+                        console.log(error.response);
+                    }
+                    );
     }
 
     viewScheduling = () => {
@@ -48,32 +54,40 @@ export default class ScheduleValidation extends React.Component {
                     <h1>Lattes</h1>
                     <h2>++</h2>
 
-                    <button type="importresume" class="b1">Importar currículo</button>
-                    <button type="schedulevalidation" class="b2">Agendar validação</button>
-                    <button onClick={this.viewScheduling} type="Schedules" class="b3">Agendamentos</button>
-                    <button type="versions" class="b4">Versões</button>
-                    <button type="export" class="b5">Exportar</button>
-                    <button type="goout" class="b6">Sair</button>
+                    <button type="importresume" className="b1">Importar currículo</button>
+                    <button type="schedulevalidation" className="b2">Agendar validação</button>
+                    <button onClick={this.viewScheduling} type="Schedules" className="b3">Agendamentos</button>
+                    <button type="versions" className="b4">Versões</button>
+                    <button type="export" className="b5">Exportar</button>
+                    <button type="goout" className="b6">Sair</button>
                 </div>
 
                 <div className="Fields">
                 <h3>Selecione uma versão do currículo, o validador, o horário e a data</h3>
-                <button onClick={this.post} type="toschedule" class="b7">Agendar</button>
+                <button onClick={this.post} type="toschedule" className="b7">Agendar</button>
                 
                     <FormGroup label='Versão' htmlFor='lab01'>
-                        <input className="form-control" type="text" id="lab01"
+                        <input className="form-control" type="text" id="lab01" placeholder='É atributo "único" no DB'
                         onChange={(e) => {this.setState({version: e.target.value})}}/>
                     </FormGroup>
-                    <FormGroup label='Validador' htmlFor='lab02'>
-                        <input className="form-control" type="text" id="lab02"
-                        onChange={(e) => {this.setState({validator: e.target.value})}}/>
+                    <FormGroup label='ID do requisitante' htmlFor='lab02'>
+                        <input className="form-control" type="number" id="lab02" placeholder='Registrado no DB --> 1'
+                        onChange={(e) => {this.setState({requesterId: e.target.value})}}/>
                     </FormGroup>
-                    <FormGroup label='Data' htmlFor='lab01' className="FieldSetSc">
-                        <input className="form-control noMargin" type="date" id="lab01"
+                    <FormGroup label='ID do validador' htmlFor='lab03'> 
+                        <input className="form-control" type="number" id="lab03" placeholder='Registrado no DB --> 2'
+                        onChange={(e) => {this.setState({validatorId: e.target.value})}}/>
+                    </FormGroup>
+                    <FormGroup label='Endereço' htmlFor='lab04'>
+                        <input className="form-control" type="text" id="lab04"
+                        onChange={(e) => {this.setState({address: e.target.value})}}/>
+                    </FormGroup>
+                    <FormGroup label='Data' htmlFor='lab05'>
+                        <input className="form-control noMargin" type="date" id="lab05"
                         onChange={(e) => {this.setState({date: e.target.value})}}/>
                     </FormGroup>
-                    <FormGroup label='Horário' htmlFor='lab02' className="FieldSetSc">
-                        <input className="form-control noMargin" type="time" id="lab02"
+                    <FormGroup label='Horário' htmlFor='lab06'>
+                        <input className="form-control noMargin" type="time" id="lab06"
                         onChange={(e) => {this.setState({time: e.target.value})}}/>
                     </FormGroup>
 
