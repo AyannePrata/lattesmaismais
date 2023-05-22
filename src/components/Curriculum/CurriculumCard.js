cc5809ae46476ab58e521f224cbba586aebccdc0
Ayanne Prata <ayanneprata7@gmail.com>
2023-05-21 23:21:24 -0300

    Changes needed to version list screen

---
+++ src/components/Curriculum/CurriculumCard.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './CurriculumCard.css';
+
+import { Button } from 'reactstrap';
+import img14 from '../../assets/images/recyclebinEmpty.svg';
+
+function CurriculumCard(props) {
+
+    <div className="box">
+        <h1 id='version'>{this.state.version}</h1>
+        <h2 id='commentary'>{this.state.commentaryToNewVersion}</h2>
+        <h3>Alterado em:</h3>
+        <h4 id='modificationDateAndTime'>{this.state.lastModification}</h4>
+        <button type="toedit" className="b1" >
+
+        </button>
+        <button type="delete" className="b2">
+            <img id="ico-delete" className="Button-delete"  border="0" src={img14} width="120" height="30" />
+        </button>
+        </div>
+}
+
+export default CurriculumCard;
--- src/screens/VersionListing/VersionListing.js
+++ src/screens/VersionListing/VersionListing.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './VersionListing.css';
-
 import VersionsService from '../../services/VersionsService';
 import FormGroup from "../../components/FormGroup/FormGroup";
 import { withRouter } from 'react-router';
 
-
-import img14 from '../../assets/images/recyclebinEmpty.svg';
+import CurriculumCard from '../../components/Curriculum/CurriculumCard';
 import LeftMenu from '../../components/Menu/LeftMenu';
 
 
@@ -23,29 +21,17 @@ class VersionListing extends React.Component {
         lastModification: "",
     }
 
+    componentDidMount() {
+        this.find();
+    }
+
 
     render() {
         return (
             <div className="Versions-Screen">
                 <LeftMenu/>
-                <div className="box">
-                    <h1 id='version'>{this.state.version}</h1>
-                    <h2 id='commentary'>{this.state.commentaryToNewVersion}</h2>
-                    <h3>Alterado em:</h3>
-                    <h4 id='modificationDateAndTime'>{this.state.lastModification}</h4>
-                    <button type="toedit" className="b1" >
-                    </button>
-                    <button type="delete" className="b2">
-                        <img id="ico-delete" className="Button-delete"  border="0" src={img14} width="120" height="30" />
-
-
-                    </button>
-
-
-                </div>
-               
-               
-               
+                <CurriculumCard/>
+              
             </div>
         )
 
